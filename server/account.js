59cd8594d8e68d7054af4796e00f34449d862ddf
upranay11 <upranay11@gmail.com>
2022-10-23 23:48:46 -0400

    started user api

---
+++ server/account.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require('mongodb');
+
+async function main() {
+    
+    const uri = "mongodb+srv://Admin:Admin@cluster0.1dx8tdy.mongodb.net/?retryWrites=true&w=majority";
+    
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+        
+    } catch (e) {
+        console.error(e);
+    } finally {
+        client.close();
+    }
+
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+    const databaseList = await client.db().admin().listDatabases();
+
+    console.log("Databases");
+    databaseList.databases.forEach(db => {
+        console.log(`- ${db.name}`);
+    })
+}
+
+async function insertAccount(client, newAccount) {
+    const result = await client.db("user input").collection("accounts").insertOne(newAccount);
+
+    console.log(`New account created with the id: ${result.insertedId}`);
+    
+}
