52cd3985fd8edcb2f739405a10f0f83ee12ae679
upranay11 <upranay11@gmail.com>
2022-10-25 18:59:52 -0400

    updated account.js
    can create, update, and read user information/account.

--- server/account.js
+++ server/account.js
@@ -9,8 +9,7 @@ async function main() {
     try {
         await client.connect();
 
-        await listDatabases(client);
-        
+
     } catch (e) {
         console.error(e);
     } finally {
@@ -30,15 +29,74 @@ async function listDatabases(client) {
     })
 }
 
-async function insertAccount(client, newAccount) {
-    const result = await client.db("user input").collection("accounts").insertOne(newAccount);
+async function insertUser(client, newUser) {
+    const result = await client.db("user").collection("Users").insertOne(newUser);
 
     console.log(`New account created with the id: ${result.insertedId}`);
     
 }
 
-async function insertAccount(client, edittedAccount) {
-    const result = await client.db("user input").collection("accounts").updateOne(edittedAccount);
+async function editUser(client, userEmail, edittedUser) {
+    const result = await client.db("user").collection("Users").updateOne({email: userEmail}, {$set: edittedUser });
 
-    console.log(`Account information editted of account with the id: ${result.insertedId}`);
+    console.log(`${result.matchCount} user matched with email criteria`);
+    console.log(`${result.modifiedCount} user profile was updated`)
+}
+
+async function findUser(client, userEmail) {
+    const result = await client.db("user").collection("Users").findOne({email: userEmail});
+
+    if (result) {
+        console.log(`Found a user with the email '${userEmail}'`);
+        console.log(result)
+    } else {
+        console.log(`No user found with the email '${userEmail}'`);
+    }
+}
+
+async function loginCheck(client, userEmail, userPassword) {
+    const associatedAccount = await client.db("user").collection("Users").findOne({email: userEmail});
+
+    if (associatedAccount) {
+
+        if (userPassword == `${associatedAccount.password}`) {
+            const passwordMatch = true;
+        } else {
+            const passwordMatch = false;
+        }
+
+        if (passwordMatch == false) {
+            console.log("Username or Password is incorrect. Please try again.");
+        }
+        
+    } else {
+        console.log("Username or Password is incorrect. Please try again.");
+    }
+}
+
+
+
+async function storeFunctions() {
+
+    await insertUser(client, {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@gmail.com",
+        password: "jd1234!",
+        number: "",
+        status: "active",
+        rememberMe: "False"
+    })
+
+    await editUser(client, "johnsmith@gmail.com", {
+        firstName: "",
+        lastName: "",
+        password: "",
+        number: "",
+        status: "",
+        rememberMe: ""
+    });
+
+    await findUser(client, "johnsmith@gmail.com");
+
 }
