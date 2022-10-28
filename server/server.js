519f10f11f2ba9b40bdf7dd8daa2c38e0f0deecb
patrick-chen8 <72107137+patrick-chen8@users.noreply.github.com>
2022-10-27 22:40:42 -0400

    updated server.js and testing new idea

--- server/server.js
+++ server/server.js
@@ -10,12 +10,19 @@ const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+
+const users = require('./routes/users');
+
 app.use(cors(corsOptions));
 app.use(express.json());
-//app.use(require('./routes/record'));
+app.use('/users', users);
 
 // Global error handling
+<<<<<<< Updated upstream
 app.use(function (err, res, _req) {
+=======
+app.use(function (err, res, _req, next) {
+>>>>>>> Stashed changes
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
@@ -32,60 +39,3 @@ dbo.connectToServer(function (err) {
         console.log(`Server is running on port: ${PORT}`);
     });
 });
-
-
-// require('dotenv').config();
-// const express = require("express");
-// const app = express();
-// const path  = require('path');
-// const PORT = process.env.PORT || 5000;
-// const cors = require('cors');
-// const corsOptions = require('./config/corsOptions');
-// const connect = require('./db/conn')
-
-// app.use(express.json());
-
-// app.use(cors(corsOptions));
-
-// app.use('/', express.static(path.join(__dirname, '/public')));
-
-// app.use('/', require('./routes/root'));
-
-// app.all('*', (req, res) => {
-//     res.status(404)
-//     if (req.accepts('html')) {
-//         res.sendFile(path.join(__dirname, 'views', '404.html'))
-//     } else if (req.accepts('json')) {
-//         res.json({message: '404 Not Found'})
-//     } else {
-//         res.type('txt').send('404 Not Found')
-//     } 
-// });
-
-// app.listen(PORT, () => {
-//     connect.connectDB();
-//     console.log(`Server running on port ${PORT}`)
-// });
-
-
-
-
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// require("dotenv").config({ path: "./config.env" });
-// const port = process.env.PORT || 5000;
-// app.use(cors());
-// app.use(express.json());
-// app.use(require("./routes/record"));
-// // get driver connection
-// const dbo = require("./db/conn");
- 
-// app.listen(port, () => {
-//   // perform a database connection when server starts
-//   dbo.connectToServer(function (err) {
-//     if (err) console.log(err);
- 
-//   });
-//   console.log(`Server is running on port: ${port}`);
-// });
