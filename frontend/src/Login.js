7ea141dd35ee95aa8464c0bfa2e3f2299cac0783
Tyler Stott <trs63653@uga.edu>
2022-10-28 07:19:39 -0400

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Redirect } from "react-router-dom";
 
 class Login extends React.Component{
 
@@ -48,7 +47,11 @@ class Login extends React.Component{
         //navigate("/");
         //this.pullData();
         this.createSession();
+<<<<<<< Updated upstream
         //<Redirect to="/home"></Redirect>
+=======
+        window.location.href = "/home";
+>>>>>>> Stashed changes
     }
 
         // const [records, setRecords] = useState([]);
