736277052e0d224c08027f6ac61e397ef889e403
Cameronwm26 <116125206+Cameronwm26@users.noreply.github.com>
2022-11-20 18:50:40 -0500

    Update ManageUsers.js

--- frontend/src/admin/ManageUsers.js
+++ frontend/src/admin/ManageUsers.js
@@ -21,8 +21,28 @@ class ManagePromos extends React.Component {
 
     }
 
-    gatherUsers() {
+    async gatherUsers() {
         // Find all users fetch request
+        console.log("gatherUsers function")
+
+        const response = await fetch("http://localhost:5000/findusers", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .catch(error => {
+            window.alert(error);
+            return;
+        });
+        const currentUsers = await response.json();
+        
+
+        console.log(currentUsers)
+        
+        this.setState({usersArray: currentUsers});
+        
+        console.log(this.state)
         
     }
 
