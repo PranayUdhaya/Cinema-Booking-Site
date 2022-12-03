9cab5dada9e4a9d6676ae320196b56b9b251e39c
upranay11 <upranay11@gmail.com>
2022-12-03 15:59:32 -0500

    fixing typo

--- server/routes/users.js
+++ server/routes/users.js
@@ -24,7 +24,7 @@ router.post("/users/verify", userController.verifyAccount);
 
 router.post("/users/findusers", userController.findAllUsers);
 
-router.post("user/forgetPassword", userController.forgetPassword);
+router.post("users/forgetPassword", userController.forgetPassword);
 
 module.exports = router;
 
